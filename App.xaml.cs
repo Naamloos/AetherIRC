f830ae64d24c4a1e062b87ff9e51034974bd5e3b
Ryan de Jonge <r.dejonge@provrex.nl>
2024-02-06 15:58:23 +0100

    Initial project push

---
+++ App.xaml.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace AetherIRC
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            base.OnStartup(e);
+            var main = new MainWindow();
+            var mainmodel = new MainWindowModel();
+            main.DataContext = mainmodel;
+            main.Show();
+        }
+    }
+}
---
+++ MainWindowModel.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AetherIRC
+{
+    public class MainWindowModel : INotifyPropertyChanged
+    {
+        public List<string> Servers
+        {
+            get => servers;
+            set
+            {
+                servers = value;
+                OnPropertyChanged();
+            }
+        }
+
+        private List<string> servers = new();
+
+        public MainWindowModel()
+        {
+            Servers.Add("Libera");
+            Servers.Add("FreeNode");
+            OnPropertyChanged();
+        }
+
+        protected void OnPropertyChanged([CallerMemberName] string name = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(name));
+        }
+
+        public event PropertyChangedEventHandler? PropertyChanged;
+    }
+}
